662c750df55c78adff3956c329ffff6aa2f6c633
yudai <kabuyouaka@gmail.com>
2019-08-11 16:00:18 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 阿部祐大 on 2019/08/11.
+//  Copyright © 2019 yudai.abe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SlideshowAppTests/SlideshowAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SlideshowAppTests.swift
+//  SlideshowAppTests
+//
+//  Created by 阿部祐大 on 2019/08/11.
+//  Copyright © 2019 yudai.abe. All rights reserved.
+//
+
+import XCTest
+@testable import SlideshowApp
+
+class SlideshowAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
